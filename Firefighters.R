8fe7560c9960f77b568d37c0c4eeedd9ad33daf9
DanielHadley <dphnrome@yahoo.com>
2015-04-01 12:43:58 -0400

    a very basic simulation: must add nuance

--- Firefighters.R
+++ Firefighters.R
@@ -22,44 +22,49 @@ hist(d$costPerShift14)
 # Most time we are not paying much overtime for missed shifts ^^^
 
 
-# Now I will group by the number of missed shifts to try and come up 
-# with an average value per missed shift
+#### This first section is to model the average OT cost per shift given how many shifts must be filled ####
 # The problem, of course, is that there are other factors
 # like sick shifts that come into play
 
-byMissed <- d %>%
-  group_by(FY13Missed) %>%
-  summarise(avg = mean(costPerShift13),
-            min = min(costPerShift13),
-            max = max(costPerShift13),
-            stDev = sd(costPerShift13))
+# First combine the data to get averages and start the model
+Thirteen <- d %>%
+  select(FY13Missed, Day, costPerShift13) %>%
+  rename(Missed = FY13Missed, cost = costPerShift13)
+
+Fourteen <- d %>%
+  select(FY14Missed, Day, costPerShift14) %>%
+  rename(Missed = FY14Missed, cost = costPerShift14)
+
+CostPerShift <- union(Thirteen, Fourteen)
+rm(Thirteen, Fourteen)
+
 
 # simple regression model to see what an average number of missed shifts will cost
-reg1 <- lm(byMissed$avg~byMissed$FY13Missed)
-plot(byMissed$avg~byMissed$FY13Missed)
+byMissed <- CostPerShift %>%
+  group_by(Missed) %>%
+  summarise(avg = mean(cost),
+            min = min(cost),
+            max = max(cost),
+            stDev = sd(cost))
+
+byMissed[1,] <- 0
+
+reg1 <- lm(byMissed$avg~byMissed$Missed)
+plot(byMissed$avg~byMissed$Missed)
 abline(reg1)
 
 byMissed$Predicted <- predict(reg1)
 
 
+## new is the key for predicting cost per shift
+x <- byMissed$Missed
+y <- x + byMissed$avg
+predict(lm(y ~ x))
+new <- data.frame(x = seq(0,27))
+reg2 <- predict(lm(y ~ x), new, se.fit = TRUE)
 
-# Now for 14
-byMissed14 <- d %>%
-  group_by(FY14Missed) %>%
-  summarise(avg = mean(costPerShift14),
-            min = min(costPerShift14),
-            max = max(costPerShift14),
-            stDev = sd(costPerShift14))
-
-# simple regression model to see what an average number of missed shifts will cost
-byMissed14[1,] <- 0
-
-reg1 <- lm(byMissed14$avg~byMissed14$FY14Missed)
-plot(byMissed14$avg~byMissed14$FY14Missed)
-abline(reg1)
-
-byMissed14$Predicted <- predict(reg1)
-
+new$CostPerMissed <- reg2$fit
+names(new)[1] <- "Missed"
 
 
 
@@ -80,13 +85,6 @@ summary(OffPeak$avgMissed)
 sd(OffPeak$avgMissed)
 hist(OffPeak$avgMissed)
 
-# Now simulate it
-OffPeakSim <- rtruncnorm(n=178, a=0.5, b=5, m=3.25, sd=1.25) 
-hist(OffPeakSim)
-
-# I round so that I can combine with averages later
-OffPeakSim <- round(OffPeakSim)
-
 
 ### A simulation of Peak
 # First see what the actual dist looks like
@@ -98,18 +96,51 @@ sd(Peak$avgMissed)
 hist(Peak$avgMissed)
 
 # Now simulate it
-PeakSim <- rtruncnorm(n=178, a=5, b=20, m=10.25, sd=3.5) 
-hist(PeakSim)
+RegularYearSim <- function(nsims){
+  for (i in 1:nsims) {
+    OffPeakSim <- rtruncnorm(n=178, a=0.5, b=5, m=3.25, sd=1.25) 
+    # I round so that I can combine with averages later
+    OffPeakSim <- round(OffPeakSim)
+    
+    PeakSim <- rtruncnorm(n=187, a=5, b=20, m=10.25, sd=3.5) 
+    # I round so that I can combine with averages later
+    PeakSim <- round(PeakSim)
+    
+    
+    OffPeakSim <- OffPeakSim  %>% as.data.frame() 
+    PeakSim <- PeakSim  %>% as.data.frame() 
+    
+    OneYear <- rbind(OffPeakSim, PeakSim)
+    names(OneYear)[1] <- "Missed"
+    
+    OneYearFinal <- merge(OneYear, new)
+    OneYearFinal$TotalCost <- OneYearFinal$Missed * OneYearFinal$CostPerMissed
+    i = sum(OneYearFinal$TotalCost)
+    print(i)
+  }
+}
 
-# I round so that I can combine with averages later
-PeakSim <- round(PeakSim)
 
-
-
-OffPeakSim <- OffPeakSim  %>% as.data.frame() 
-PeakSim <- PeakSim  %>% as.data.frame() 
-
-OneYear <- rbind(OffPeakSim, PeakSim)
-names(OneYear)[1] <- "FY14Missed"
-
-test <- merge(OneYear, byMissed14)
+ShiftedYearSim <- function(nsims, avgPeak, avgOffPeak){
+  for (i in 1:nsims) {
+    OffPeakSim <- rtruncnorm(n=178, a=0.5, b=5, m=avgOffPeak, sd=1.25) 
+    # I round so that I can combine with averages later
+    OffPeakSim <- round(OffPeakSim)
+    
+    PeakSim <- rtruncnorm(n=187, a=5, b=20, m=avgPeak, sd=3.5) 
+    # I round so that I can combine with averages later
+    PeakSim <- round(PeakSim)
+    
+    
+    OffPeakSim <- OffPeakSim  %>% as.data.frame() 
+    PeakSim <- PeakSim  %>% as.data.frame() 
+    
+    OneYear <- rbind(OffPeakSim, PeakSim)
+    names(OneYear)[1] <- "Missed"
+    
+    OneYearFinal <- merge(OneYear, new)
+    OneYearFinal$TotalCost <- OneYearFinal$Missed * OneYearFinal$CostPerMissed
+    i = sum(OneYearFinal$TotalCost)
+    print(i)
+  }
+}
